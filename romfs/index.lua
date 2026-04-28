bd8b3259100afb5319d9198d3eea353c8baf6b9b
Luftwaffles <sunnyhoang314@gmail.com>
2026-04-28 16:33:55 -0600

    adde theme song + good/bad ending songs

--- romfs/index.lua
+++ romfs/index.lua
@@ -30,6 +30,7 @@ if System.currentDirectory() == "/" then
 	System.currentDirectory("romfs:/")
 end
 TITLEi=0
+THEME_SONG = Audio.open(System.currentDirectory().."ASSETS\audio\01. Glory to Arstotzka (Main Theme).mp3")
 NORMAL_FONT = Graphics.loadImage(System.currentDirectory().."ASSETS/FONT/NORMAL_FONT.png")
 SMALL_FONT = Graphics.loadImage(System.currentDirectory().."ASSETS/FONT/SMALL_FONT.png")
 DIGITS = Graphics.loadImage(System.currentDirectory().."ASSETS/FONT/DIGITS.png")
